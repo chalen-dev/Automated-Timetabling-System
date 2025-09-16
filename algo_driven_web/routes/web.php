90e448297fe70bda0ad51cd224d56ce135b2ec77
chalen-dev <c.lusaya.142396.tc@umindanao.edu.ph>
2025-09-16 12:57:02 +0800

    - reorganize file structure in resources/views - creation of app.blade.php template, navigation, footer - login, register, and their routing

--- algo_driven_web/routes/web.php
+++ algo_driven_web/routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('test');
+    return view('home');
+});
+
+Route::get('/login', function () {
+    return view('auth.login'); // resources/views/auth/login.blade.php
+});
+
+Route::get('/register', function () {
+    return view('auth.register'); // resources/views/auth/register.blade.php
 });
