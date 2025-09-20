33db08939c27be03fa2e8713e363c36f0d8368e6
chalen-dev <c.lusaya.142396.tc@umindanao.edu.ph>
2025-09-20 22:00:31 +0800

    add DashboardController.php

---
+++ myapp/app/Http/Controllers/DashboardController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+
+    public function __construct()
+    {
+        //Literally apply middleware auth to all methods in this controller, cause you should
+        $this->middleware('auth');
+    }
+
+    public function showDashboard(){
+
+        //Get the authenticated user
+        $user = auth()->user();
+
+        //Display dashboard, while passing in the details of the authenticated user to the blade template/component
+        return view('pages.dashboard', compact('user'));
+    }
+}
--- myapp/routes/web.php
+++ myapp/routes/web.php
@@ -1,20 +1,38 @@
 <?php
 
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\HomeController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
 
-//Homepage or Dashboard, depending on authentication status
-Route::get('/', [HomeController::class, 'index']);
+// Default Page, either Homepage or Dashboard, depending on authentication status
+Route::get('/', [WelcomeController::class, 'index'])->name('welcome');
 
 // Register
-Route::get('/register', [AuthController::class, 'showRegisterForm'])->middleware('guest')->name('register.form');
-Route::post('/register', [AuthController::class, 'register'])->middleware('guest')->name('register');
+Route::get('/register', [AuthController::class, 'showRegisterForm'])
+    ->middleware('guest')
+    ->name('register.form');
+
+Route::post('/register', [AuthController::class, 'register'])
+    ->middleware('guest')
+    ->name('register');
 
 // Login
-Route::get('/login', [AuthController::class, 'showLoginForm'])->middleware('guest')->name('login.form');
-Route::post('/login', [AuthController::class, 'login'])->middleware('guest')->name('login');
+Route::get('/login', [AuthController::class, 'showLoginForm'])
+    ->middleware('guest')
+    ->name('login.form');
+
+Route::post('/login', [AuthController::class, 'login'])
+    ->middleware('guest')
+    ->name('login');
 
 // Logout
-Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth')->name('logout');
+Route::post('/logout', [AuthController::class, 'logout'])
+    ->middleware('auth')
+    ->name('logout');
+
+//Dashboard
+Route::get('/dashboard', [DashboardController::class, 'showDashboard'])
+    ->middleware('auth')
+    ->name('pages.dashboard');
