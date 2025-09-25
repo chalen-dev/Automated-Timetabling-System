7f838a92e02eed39f575be3c3a0a0358415d3643
chalen-dev <c.lusaya.142396.tc@umindanao.edu.ph>
2025-09-25 20:32:07 +0800

    add sample key value pairs for course controller, for display at view

--- myapp/app/Http/Controllers/CourseController.php
+++ myapp/app/Http/Controllers/CourseController.php
@@ -10,8 +10,49 @@ class CourseController extends Controller
 
         //Test Code Start
         $courses = [
-            ['course_title' => 'IT 12', 'id' => 1],
-            ['course_title' => 'IT 14', 'id' => 2],
+            [
+                'course_id' => 1,
+                'course_title' => 'CCE 101/L',
+                'course_name' => 'Introduction to Computing',
+                'course_type' => 'major',
+                'class_hours' => 2,
+                'total_lecture_class_days' => 2,
+                'total_laboratory_class_days' => 2,
+                'unit_load' => 4.5,
+            ],
+
+            [
+                'course_id' => 2,
+                'course_title' => 'GE 2',
+                'course_name' => 'Purposive Communication and Interactive Learning',
+                'course_type' => 'minor',
+                'class_hours' => 1,
+                'total_lecture_class_days' => 3,
+                'total_laboratory_class_days' => 0,
+                'unit_load' => 3,
+            ],
+
+            [
+                'course_id' => 3,
+                'course_title' => 'PAHF 1',
+                'course_name' => 'Movement Competency Training',
+                'course_type' => 'pe',
+                'class_hours' => 4,
+                'total_lecture_class_days' => 1,
+                'total_laboratory_class_days' => 0,
+                'unit_load' => 2,
+            ],
+
+            [
+                'course_id' => 4,
+                'course_title' => 'NSTP 1',
+                'course_name' => 'National Service Training Program 1',
+                'course_type' => 'nstp',
+                'class_hours' => 4,
+                'total_lecture_class_days' => 1,
+                'total_laboratory_class_days' => 0,
+                'unit_load' => 2,
+            ],
         ];
         //Test code end
 
