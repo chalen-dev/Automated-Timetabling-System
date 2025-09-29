9c350920402665edcee56299330cb0fcad895bd7
chalen-dev <c.lusaya.142396.tc@umindanao.edu.ph>
2025-09-29 11:00:03 +0800

    JEI GOZON Create and Edit Room (adding division for arrangement)

--- myapp/resources/views/records/Rooms/create.blade.php
+++ myapp/resources/views/records/Rooms/create.blade.php
@@ -3,39 +3,49 @@
 @section('title', 'Create Room')
 
 @section('content')
-    <h1>Create Room</h1>
-    <form action="{{route('records.rooms.store')}}" method="POST">
-        @csrf
+    <div class="flex flex-col gap-5 justify-center items-center">
+        <h1>Create Room</h1>
+        <form action="{{route('records.rooms.store')}}" method="POST">
+            @csrf
+            <div class="flex flex-col gap-5">
+                <div class="flex flex-row gap-20">
+                    <x-input.text
+                        label="Room Name"
+                        name="room_name"
+                    />
 
-        <x-input.text
-            label="Room Name"
-            name="room_name"
-        />
+                    <x-input.select
+                        label="Room Type"
+                        name="room_type"
+                        :options="$roomTypeOptions"
+                        default=""
+                    />
 
-        <x-input.select
-            label="Room Type"
-            name="room_type"
-            :options="$roomTypeOptions"
-            default=""
-        />
+                    <x-input.select
+                        label="Course Type Exclusive To"
+                        name="course_type_exclusive_to"
+                        :options="$courseTypeExclusiveToOptions"
+                        default="none"
+                    />
 
-        <x-input.select
-            label="Course Type Exclusive To"
-            name="course_type_exclusive_to"
-            :options="$courseTypeExclusiveToOptions"
-            default="none"
-        />
+                    <x-input.number
+                        label="Room Capacity"
+                        name="room_capacity"
+                        :default="0"
+                        :min="0"
+                        :max="50"
+                        :step="1"
+                    />
+                </div>
+                <div class="flex justify-between">
+                    <a href="{{route('records.rooms.index')}}">Back</a>
+                    <button type="submit">Create</button>
+                </div>
+            </div>
 
-        <x-input.number
-            label="Room Capacity"
-            name="room_capacity"
-            :default="0"
-            :min="0"
-            :max="50"
-            :step="1"
-        />
 
-        <button type="submit">Create</button>
-    </form>
-    <a href="{{route('records.rooms.index')}}">Back</a>
+
+        </form>
+
+    </div>
 @endsection
--- myapp/resources/views/records/Rooms/edit.blade.php
+++ myapp/resources/views/records/Rooms/edit.blade.php
@@ -3,44 +3,52 @@
 @section('title', 'Edit Room')
 
 @section('content')
-    <h1>Edit Room</h1>
-    <form action="{{route('records.rooms.update', $room)}}" method="POST">
-        @csrf
-        @method('PUT')
+    <div class="flex flex-col gap-5 justify-center items-center">
+        <h1>Edit Room</h1>
+        <form action="{{route('records.rooms.update', $room)}}" method="POST">
+            @csrf
+            @method('PUT')
+            <div class="flex flex-col gap-5">
+                <div class="flex flex-row gap-5">
+                    <x-input.text
+                        label="Room Name"
+                        name="room_name"
+                        :value="old('room_name', $room->room_name)"
+                    />
 
-        <x-input.text
-            label="Room Name"
-            name="room_name"
-            :value="old('room_name', $room->room_name)"
-        />
+                    <x-input.select
+                        label="Room Type"
+                        name="room_type"
+                        :options="$roomTypeOptions"
+                        default=""
+                        :value="old('room_type', $room->room_type)"
+                    />
 
-        <x-input.select
-            label="Room Type"
-            name="room_type"
-            :options="$roomTypeOptions"
-            default=""
-            :value="old('room_type', $room->room_type)"
-        />
+                    <x-input.select
+                        label="Course Type Exclusive To"
+                        name="course_type_exclusive_to"
+                        :options="$courseTypeExclusiveToOptions"
+                        :value="old('course_type_exclusive_to', $room->course_type_exclusive_to)"
+                    />
 
-        <x-input.select
-            label="Course Type Exclusive To"
-            name="course_type_exclusive_to"
-            :options="$courseTypeExclusiveToOptions"
-            :value="old('course_type_exclusive_to', $room->course_type_exclusive_to)"
-        />
+                    <x-input.number
+                        label="Room Capacity"
+                        name="room_capacity"
+                        :default="0"
+                        :min="0"
+                        :max="50"
+                        :step="1"
+                        :value="old('room_capacity', $room->room_capacity)"
+                    />
+                </div>
+                <div class="flex justify-between">
+                    <a href="{{route('records.rooms.index')}}">Back</a>
+                    <button type="submit">Update</button>
+                </div>
 
-        <x-input.number
-            label="Room Capacity"
-            name="room_capacity"
-            :default="0"
-            :min="0"
-            :max="50"
-            :step="1"
-            :value="old('room_capacity', $room->room_capacity)"
-        />
+            </div>
+        </form>
 
-        <button type="submit">Update</button>
+    </div>
 
-    </form>
-    <a href="{{route('records.rooms.index')}}">Back</a>
 @endsection
