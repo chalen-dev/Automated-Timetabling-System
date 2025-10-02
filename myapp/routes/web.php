23f13e4d46af5d56cf339857183a4a75f66d5325
chalen-dev <c.lusaya.142396.tc@umindanao.edu.ph>
2025-10-02 23:17:19 +0800

    implement route prefixes for cleanliness

--- myapp/routes/web.php
+++ myapp/routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\RoomController;
 use App\Http\Controllers\RoomExclusiveDayController;
 use App\Http\Controllers\SpecializationController;
 use App\Http\Controllers\TimetableController;
+use App\Http\Controllers\TimetableEditingPaneController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\WelcomeController;
 use App\Http\Middleware\Authenticate;
@@ -20,6 +21,7 @@ Route::get('/', [WelcomeController::class, 'index'])
 
 // Guest Routes (Unauthenticated)
 Route::middleware('guest')->group(function () {
+
     // Register
     Route::get('/register', [UserController::class, 'showRegisterForm'])
         ->name('register.form');
@@ -41,24 +43,35 @@ Route::middleware([Authenticate::class])->group(function () {
     Route::post('/logout', [UserController::class, 'logout'])
         ->name('logout');
 
-    // Timetable Routes (timetable list, the DASHBOARD)
-    Route::resource('timetables', TimetableController::class);
+    // Timetable and its nested routes NOTE: the TIMETABLE LIST act as DASHBOARD
+    Route::prefix('timetables')->name('timetables.')->group(function() {
+            //Timetable List(act as dashboard)
+            Route::resource('/', TimetableController::class);
+            //Timetabling Editing Pane
+            Route::resource('timetable-editing-pane', TimetableEditingPaneController::class)->only('index');
+    });
 
-    // courses Routes
+    // Courses Routes
     Route::resource('courses', CourseController::class);
 
-    // Professor Routes
-    Route::resource('professors', ProfessorController::class);
-        // Specialization Routes (scoped to professors)
-        Route::resource('professors.specializations', SpecializationController::class)->only('create', 'index', 'store', 'destroy');
+    //Professor and nested routes
+    Route::prefix('professors')->name('professors.')->group(function() {
+        //Professor List
+       Route::resource('/', ProfessorController::class);
+       //Specialization List
+       Route::resource('specializations', SpecializationController::class)->only('create', 'index', 'store', 'destroy');
+    });
 
     // Academic Program Routes
     Route::resource('academic-programs', AcademicProgramController::class);
 
-    // Room Routes
-    Route::resource('rooms', RoomController::class);
-        // Room Exclusive Day Routes (scoped to rooms)
-        Route::resource('rooms.room-exclusive-days', RoomExclusiveDayController::class)->only('index', 'create', 'store', 'destroy');
+    // Room and nested routes
+    Route::prefix('rooms')->name('rooms.')->group(function() {
+        //Room List
+        Route::resource('/', RoomController::class);
+        //Room Exclusive Day List
+        Route::resource('room-exclusive-days', RoomExclusiveDayController::class)->only('index', 'create', 'store', 'destroy');
+    });
 
 });
 
