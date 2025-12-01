6a20f51104aa861e6cf87737d3bfe6d3f7068462
Jei <j.gozon.143104.tc@umindanao.edu.ph>
2025-12-01 16:55:11 +0800

    Partially Restructured the Log-In/Sign-up page Alternatively changed the background as maroon due to UMV.png wont show as background in app.css file

--- myapp/resources/views/livewire/partials/headers.blade.php
+++ myapp/resources/views/livewire/partials/headers.blade.php
@@ -8,23 +8,11 @@
                 @if (request()->routeIs('home') || request()->routeIs('login.form'))
                     <div class="flex flex-row gap-4 items-center">
                         <img src="{{asset('umtc_logo.png')}}" class="w-15 h-15" alt="UMTC Logo" >
-                        <h2 class="font-bold text-[#ffffff]">FaculTime</h2>
                     </div>
-                    <a href="{{ route('register.form') }}" class="content-center">
-                        <button type=button class="bg-white text-[#5E0B0B] px-4 py-2 rounded-lg shadow h-12 flex items-center cursor-pointer">
-                            <span>Sign Up</span>
-                        </button>
-                    </a>
                 @elseif (request()->routeIs('register.form'))
                     <div class="flex flex-row gap-4 items-center">
                         <img src="{{asset('umtc_logo.png')}}" class="w-15 h-15" alt="UMTC Logo" >
-                        <h2 class="font-bold text-[#ffffff]">FaculTime</h2>
                     </div>
-                    <a href="{{ route('login.form') }}" class="content-center">
-                        <button type=button class="bg-white text-[#5E0B0B] px-4 py-2 rounded-lg shadow h-12 flex items-center cursor-pointer">
-                            <span>Sign In</span>
-                        </button>
-                    </a>
                 @endif
             </div>
         </nav>
